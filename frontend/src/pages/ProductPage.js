277dcb577999ee803ff147e7e69c8a125e67cf6e
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-12-31 15:23:47 -0300

    Product Detail Page added

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
+import products from '../products';
 
 const ProductPage = () => {
-  return <div>Product</div>;
+  const params = useParams();
+  const product = products.find((p) => p._id === params.id);
+
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Atrás
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Precio: $ {product.price}</ListGroup.Item>
+            <ListGroup.Item>Descripción: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Precio</Col>
+                  <Col>
+                    <strong>$ {product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Stock:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'Disponible' : 'Sin stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item className='d-grid'>
+                <Button
+                  disabled={product.countInStock === 0}
+                  variant='primary'
+                  className='text-uppercase fw-bold'
+                  type='button'>
+                  Agregar al Carrito
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductPage;
