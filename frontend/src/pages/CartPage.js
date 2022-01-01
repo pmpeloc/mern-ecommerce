04beddac6bfd46d29bbaa898642595b9c0887bea
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-01 12:44:52 -0300

    Add to cart funcionality added

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams, useLocation, useNavigate } from 'react-router-dom';
+import Message from '../components/Message';
+import { addToCart } from '../redux/actions/cartActions';
 
 const CartPage = () => {
+  const { id: productId } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const qty = new URLSearchParams(location.search).get('qty');
+
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>Cart</div>;
 };
 
