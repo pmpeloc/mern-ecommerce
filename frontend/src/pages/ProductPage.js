c6375860959d9249d9c3a9bda839ba1a01a67886
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-01 12:02:27 -0300

    Select Qty and Add Cart added

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,7 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
-import { Link, useParams } from 'react-router-dom';
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Image,
+  ListGroup,
+  Row,
+} from 'react-bootstrap';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import Rating from '../components/Rating';
 import { listProductsDetails } from '../redux/actions/productActions';
 import Loader from '../components/Loader';
@@ -9,6 +17,9 @@ import Message from '../components/Message';
 
 const ProductPage = () => {
   const params = useParams();
+  const navigate = useNavigate();
+
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
@@ -17,6 +28,10 @@ const ProductPage = () => {
   useEffect(() => {
     dispatch(listProductsDetails(params.id));
   }, [dispatch, params]);
+
+  const addToCartHandler = () => {
+    navigate(`/cart/${params.id}?qty=${qty}`);
+  };
 
   return (
     <>
@@ -68,8 +83,28 @@ const ProductPage = () => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}>
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item className='d-grid'>
                   <Button
+                    onClick={addToCartHandler}
                     disabled={product.countInStock === 0}
                     variant='primary'
                     className='text-uppercase fw-bold'
--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import CartPage from '../pages/CartPage';
 import HomePage from '../pages/HomePage';
 import ProductPage from '../pages/ProductPage';
 
@@ -15,6 +16,10 @@ const AppRouter = () => {
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/product/:id' element={<ProductPage />} />
+            <Route path='/cart'>
+              <Route path=':id' element={<CartPage />} />
+              <Route path='' element={<CartPage />} />
+            </Route>
             <Route
               path='*'
               element={
