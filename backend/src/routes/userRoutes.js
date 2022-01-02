0801f33172a2da8518f77a0ba33b67ec81d6ba7e
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-02 12:21:27 -0300

    Update profile endpoint added

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
@@ -11,5 +12,6 @@ const router = Router();
 router.post('/', registerUser);
 router.post('/login', authUser);
 router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 export default router;
