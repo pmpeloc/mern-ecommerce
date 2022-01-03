31596603dc3fe7db60400ab4daca940e7218fdff
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-03 16:41:27 -0300

    Admin Middleware & Get users enpoint added

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -93,3 +93,11 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
+
+// @desc    Get all users
+// @route   PUT /api/users
+// @access  Private/Admin
+export const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,14 +2,16 @@ import { Router } from 'express';
 import {
   authUser,
   getUserProfile,
+  getUsers,
   registerUser,
   updateUserProfile,
 } from '../controllers/userController.js';
-import { protect } from '../middlewares/authMiddleware.js';
+import { protect, admin } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.post('/', registerUser);
+router.get('/', protect, admin, getUsers);
 router.post('/login', authUser);
 router.get('/profile', protect, getUserProfile);
 router.put('/profile', protect, updateUserProfile);
