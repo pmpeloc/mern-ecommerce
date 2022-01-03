7fdb0734a28c66719cbb259498394abba0533c8d
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-03 12:22:25 -0300

    Put paid order endpoint added

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.post('/', protect, addOrderItems);
 router.get('/:id', protect, getOrderById);
+router.put('/:id/pay', protect, updateOrderToPaid);
 
 export default router;
--- frontend/src/pages/OrderPage.js
+++ frontend/src/pages/OrderPage.js
@@ -25,8 +25,10 @@ const OrderPage = () => {
   const orderId = params.id;
 
   useEffect(() => {
-    dispatch(getOrderDetails(orderId));
-  }, [dispatch, orderId]);
+    if (!order || order._id !== orderId) {
+      dispatch(getOrderDetails(orderId));
+    }
+  }, [dispatch, order, orderId]);
 
   return loading ? (
     <Loader />
