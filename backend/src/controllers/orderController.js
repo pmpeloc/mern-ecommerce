0c18241c519fd1e4c4062425f66c35b7ff9c031f
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-03 10:41:22 -0300

    Get order by ID endpoint added

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -33,3 +33,19 @@ export const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder);
   }
 });
+
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+export const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+  if (order && (req.user.isAdmin || order.user._id.equals(req.user._id))) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.post('/', protect, addOrderItems);
+router.get('/:id', protect, getOrderById);
 
 export default router;
