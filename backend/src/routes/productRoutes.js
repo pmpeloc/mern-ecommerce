a131c790f63c6cbc6fac329660b6ab39d4abf4f7
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-05 13:08:20 -0300

    Create and Update product endpoints added

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -3,13 +3,17 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from '../controllers/productController.js';
 import { admin, protect } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.get('/', getProducts);
+router.post('/', protect, admin, createProduct);
 router.get('/:id', getProductById);
 router.delete('/:id', protect, admin, deleteProduct);
+router.put('/:id', protect, admin, updateProduct);
 
 export default router;
