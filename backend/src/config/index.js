617f3d62674a98600d18282d1d0acc33814abd0c
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-09 16:23:53 -0300

    fix config.js

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 // Set the NODE_ENV to 'development' by default
 // process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const envFound = dotenv.config();
-if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+// const envFound = dotenv.config();
+// if (envFound.error) {
+//   // This error should crash whole process
+//   throw new Error("⚠️  Couldn't find .env file  ⚠️");
+// }
 
 export default {
   nodeEnv: process.env.NODE_ENV,
